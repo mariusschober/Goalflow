47ab9a16d299c6935d1bfb141ed5cbc011754376
Marius <ms@mariusschober.com>
2026-08-28 15:53:40 +0100

    fix: use native pointer event consumption API

--- android-native/app/src/main/java/com/mariusschober/goalflow/nativeapp/ui/GoalflowRoot.kt
+++ android-native/app/src/main/java/com/mariusschober/goalflow/nativeapp/ui/GoalflowRoot.kt
@@ -94,7 +94,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
