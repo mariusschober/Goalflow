54ec4ef0151db4cd6bfac232e42c12048a9f8f4a
Marius <ms@mariusschober.com>
2026-08-28 16:26:08 +0100

    test: cover native focus session recovery

--- android-native/app/src/test/java/com/mariusschober/goalflow/nativeapp/data/GoalflowFocusSessionStoreTest.kt
+++ android-native/app/src/test/java/com/mariusschober/goalflow/nativeapp/data/GoalflowFocusSessionStoreTest.kt
@@ -6,7 +6,10 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class GoalflowFocusSessionStoreTest {
     private lateinit var context: Context
 
